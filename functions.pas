504560f61f945517d3e63792224f3f93d47878e0
Raf20076 <71918459+Raf20076@users.noreply.github.com>
2021-06-13 16:49:55 +0200

    Update functions.pas

--- functions.pas
+++ functions.pas
@@ -68,8 +68,8 @@ begin
 function FindInMemo(AMemo: TRichMemo; AString: String; StartPos: Integer): Integer;
 {Find clicked error (word) from ListBoxErrors and highligh it}
 begin
-  //Result := PosEx(AString, AMemo.Text, StartPos);
-  Result := UTF8Pos(AString, AMemo.Text, StartPos);
+  Result := PosEx(AString, AMemo.Text, StartPos);
+  //Result := UTF8Pos(AString, AMemo.Text, StartPos);
   If Result > 0 then
   begin
     AMemo.SelStart := UTF8Length(PChar(AMemo.Text), Result -1);
